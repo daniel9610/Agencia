6b7c2c2d7a56cd608164a2fe320cd5ae224a308c
Ricardo <ricardo.franco>
2020-10-05 09:34:45 -0500

    Change on brief

--- resources/views/etapas/brief.blade.php
+++ resources/views/etapas/brief.blade.php
@@ -28,4 +28,36 @@
 </form>
 </div>
 <a class="btn btn-primary" href="/campaniaetapas/{{$campania_id}}">Regresar</a>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <!-- Nav pills -->
+            <ul class="nav nav-pills">
+            @foreach ($estados_brief as $estado)
+                @if($estado_actual == $estado->id)
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toggle="pill" href="#menu{{$estado->id}}">{{$estado->nombre}}</a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="pill" href="#menu{{$estado->id}}">{{$estado->nombre}}</a>
+                    </li>
+                @endif
+            @endforeach
+            </ul>
+            <div class="tab-content">
+            @foreach ($estados_brief as $estado)
+                @if($estado_actual == $estado->id)
+                    <div class="tab-pane container active" id="menu{{$estado->id}}">{{$estado->nombre}}</div>
+                @else
+                    <div class="tab-pane container fade" id="menu{{$estado->id}}">{{$estado->nombre}}</div>
+                @endif
+            @endforeach
+            </div>
+        </div>
+        <div class="col-md-12">
+            <a class="btn btn-primary mr-auto btn-lg btn-block" href="/campaniaetapas/{{$campania_id}}"><i class="fas fa-arrow-left"></i> Atrás</a>
+        </div>
+    </div>
+</div>
 @stop
